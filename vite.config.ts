d617747fb75929f57b837802fe02f7b7c71b7af9
dooyongLee <dooyong86@gmail.com>
2025-06-03 13:11:24 +0900

    설정파일 최적화

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { compression } from 'vite-plugin-compression2'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    compression(),
-    visualizer(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -19,18 +13,6 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      '/api/real-estate': {
-        target: 'http://openapi.molit.go.kr',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/real-estate/, ''),
-      },
-    },
   },
   build: {
     outDir: 'dist',
@@ -40,11 +22,9 @@ export default defineConfig({
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
           mui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
-          utils: ['axios', 'zustand', 'react-hook-form'],
         },
       },
     },
-    chunkSizeWarningLimit: 1000,
     minify: 'terser',
     terserOptions: {
       compress: {
